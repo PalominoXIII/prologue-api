da05989b5ae87a0df79181cff68d9f8cc5da42f2
Edson Jonas <ejedsonomaximo8@gmail.com>
2025-07-06 19:01:30 -0300

    Ajuste conexão PlanetScale com SSL

--- db.js
+++ db.js
@@ -5,14 +5,17 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
-db.connect((err) => {
+db.connect(err => {
     if (err) {
         console.error('Erro ao conectar no banco:', err);
     } else {
-        console.log('Conectado ao banco MySQL');
+        console.log('Conexão com o banco de dados estabelecida');
     }
 });
 
